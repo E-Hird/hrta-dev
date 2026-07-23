0a5e35f07ec4d0d5b66d1a624e46dd43c2e03a99
E. Hird <flds51@durham.ac.uk>
2026-07-23 11:45:32 +0100

    Display incoming requests

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,19 @@
 
 export default {
-  async fetch(request, env, ctx) {
-    console.info({ message: 'Hello World Worker received a request!' }); 
-    return new Response('Hello World!');
-  }
+	async fetch(request, env, ctx) {
+		if (request.method !== "POST") {
+			return new Response("Method not allowed", { status: 405 });
+		}
+
+		const origin = request.headers.get("Origin");
+		if (origin !== "https://www.hrtalentalliance.com") {
+			return new Response("Forbidden", { status : 403 });
+		}
+
+		const formData = await request.json();
+
+		console.log(formData);
+
+		return new Response("Ok", { status: 200 });
+	}
 };
